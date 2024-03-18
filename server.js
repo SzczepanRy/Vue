f421cd2bd3a14bcbb1667fb72d8ce4a965844610
4ic2 <szep.rydzewski@gmail.com>
2024-03-18 09:00:02 +0100

    A

--- server.js
+++ server.js
@@ -4,10 +4,39 @@ const app = express()
 
 app.use(express.static("./lessons"))
 
+
 app.get("/", (req, res) => {
 
     res.sendFile(__dirname + "/index.html")
 })
+
+const delData = {
+    produkty: [
+        "pr1",
+        "pr2",
+        "pr3",
+        "pr4",
+        "pr5",
+        "pr6",
+        "pr7",
+    ],
+    delivery: [
+        { name: 'inpost', src: 'gfx/inpost.png' },
+        { name: 'dhl', src: 'gfx/dhl.png' },
+        { name: 'dpd', src: 'gfx/dpd.png' },
+    ],
+    payment: [
+        { name: 'visa', src: 'gfx/visa.png' },
+        { name: 'mastercard', src: 'gfx/master.png' },
+        { name: 'blik', src: 'gfx/blik.png' },
+    ]
+}
+
+app.get("/data", (req, res) => {
+
+    res.json(delData)
+})
+
 app.get("/load", (req, res) => {
     let data = {}
     folders = fs.readdirSync('./lessons')
